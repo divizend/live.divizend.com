f60c4e1f2daea973425ef65132f68323890bb072
Julian Nalenz <julian.nalenz@divizend.com>
2025-03-31 11:34:11 +0100

    fix: wording

--- app/page.tsx
+++ app/page.tsx
@@ -369,7 +369,7 @@ export default function Home() {
                   >
                     <Button className="w-full bg-orange-100 hover:bg-orange-200 text-orange-900 border-orange-200 dark:bg-orange-900/20 dark:hover:bg-orange-900/30 dark:text-orange-100 dark:border-orange-800 cursor-pointer">
                       <span className="flex justify-center w-full">
-                        Propose Development Ideas
+                        Propose Development Projects
                       </span>
                     </Button>
                   </Link>
